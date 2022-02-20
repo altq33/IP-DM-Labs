9ddb4f54d977a02edd1396944fedd8d55ae57af5
Dmitry Chankov <91803377+altq33@users.noreply.github.com>
2022-02-20 23:23:46 +0400

    Update main.js

--- IPLabs/src/scripts/main.js
+++ IPLabs/src/scripts/main.js
@@ -1,7 +1,6 @@
 let burger = document.querySelector('.burger-menu');
 let menu = document.querySelector('.buttons-container');
 let back = document.querySelector('body');
-let header__list = document.querySelector('.header__list');
 
 burger.onclick = function(){;
     menu.classList.toggle('active');
