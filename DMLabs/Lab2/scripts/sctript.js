838f4788d3bfe7143153438f4ac29fcd39b14a7d
Дмитрий Чанков <chankovbelka123@yandex.ru>
2022-02-21 22:01:42 +0400

    сделал половину лабы2

--- DMLabs/Lab2/scripts/sctript.js
+++ DMLabs/Lab2/scripts/sctript.js
@@ -1,5 +1,35 @@
 let errorMessage = ""; 
 
+function MultiplyMatrix(A, B)
+{
+    let C = [];
+    for (var i = 0; i < 4; i++) {
+        C[i] = []; 
+    }
+    for (var i = 0; i < 4; i++) {
+         for (var j = 0; j < 4; j++) { 
+             let t = 0;
+             for (var k = 0; k < 4; k++) {
+                 t += A[ j ][k]*B[k][i];
+             }
+             C[j][i] = t;
+        }
+     }
+    return C;
+}
+
+function TransMatrix(matrix)      // Транспонирование матрицы 
+{
+    let tMatrix = [];
+    for (let i = 0; i < 4; i++) { 
+       tMatrix[i] = [];
+        for (let j = 0; j < 4; j++) {
+           tMatrix[i][j] = matrix[j][i];
+       } 
+     }
+    return tMatrix;
+}
+
 function isValidate(arr) {
     errorMessage = ""; 
     for(let i = 0; i < arr.length; i++) {
@@ -19,13 +49,53 @@ function isValidate(arr) {
 }
 
 function Calculation() {  
+    let refl = true;
+    let halfRefl = false;
+    let sym = true; 
+    let antiSym = true; 
     let matrixArray = document.querySelector(".enter").value.split("\n");
     for(let i = 0; i < matrixArray.length; i++) {
         matrixArray[i] = matrixArray[i].replace(/ +/g, ' ').trim();
         matrixArray[i] = matrixArray[i].split(" ");
     }
     if (isValidate(matrixArray)) {
-        // здесь будет логика
+        let tempMatrix = MultiplyMatrix(TransMatrix(matrixArray), matrixArray);
+        /* Переписать 
+        for(let i = 0; i < 4; i++) { 
+            for(let j = 0; j < 4; j++) {
+                if(i === j) { 
+                    if (matrixArray[i][j] != "1") {
+                        refl = false;  
+                    }
+                    if (matrixArray[i][j] === "1") {
+                        halfRefl = true;  
+                    }                  
+                }else {
+                    if (matrixArray[i][j] != matrixArray[j][i]) { 
+                        sym = false; 
+                    } 
+                    if(tempMatrix[i][j] == 1) {
+                        antiSym = false;
+                    }              
+                }
+            }      
+        }*/
+        if(refl) {
+            document.querySelector("#refl").innerHTML = "Данная матрица рефлескивна";
+        }else {
+            if (halfRefl) { 
+                document.querySelector("#refl").innerHTML = "Данная матрица косорефлескивна";
+            }else {
+                document.querySelector("#refl").innerHTML = "Данная матрица антирефлексивна";
+            }
+        }
+        if(sym) {
+            document.querySelector("#sym").innerHTML = "Данная матрица симметрична";
+        }else {
+            if(antiSym) {
+                 document.querySelector("#sym").innerHTML = "Данная матрица асимметрична";
+            }      
+        }
     }else {
         alert(errorMessage);
     }
